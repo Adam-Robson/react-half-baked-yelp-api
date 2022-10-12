456298e7a24131ec9a1eee7112d2a0c51358c17a
Adam Robson <adamray312@gmail.com>
2022-10-11 17:10:40 -0700

    add state and handler function to Appjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react';
-import './App.css';
 import { RestaurantListItem } from './components/RestaurantListItem';
 import { fetchBusinesses } from './services/yelp';
+import './App.css';
 
 function App() {
   const [businesses, setBusinesses] = useState([]);
+  const [zip, setZip] = useState('');
+  const [search, setSearch] = useState('');
   const [loading, setLoading] = useState(true);
-
-  // TODO -- add state for zip / search and add event listeners to the inputs
-
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchBusinesses();
@@ -18,7 +17,10 @@ function App() {
     fetchData();
   }, []);
 
-  // TODO -- add event for button click to handle calling fetchBusinesses with zip / search
+  const handleBusinesses = async () => {
+    const response = await fetchBusinesses(zip, search);
+    setBusinesses(response);
+  };
 
   return (
     <div className="App">
@@ -26,18 +28,17 @@ function App() {
       <div className="query-form">
         <div className="form-control">
           <label>Zip:</label>
-          <input type="text" placeholder="zip" />
+          <input type="text" placeholder="zip" value={ zip } onChange={ (e) => setZip(e.target.value) } />
         </div>
         <div className="form-control">
           <label>Query:</label>
-          <input type="text" placeholder="Search..." />
+          <input type="text" placeholder="search" value={ search } onChange={ (e) => setSearch(e.target.value) } />
+          <button onClick={ handleBusinesses }>search</button>
         </div>
-        <button>Search</button>
       </div>
-      {loading && <div className="loader"></div>}
-      {!loading && businesses.map((b) => <RestaurantListItem key={b.id} {...b} />)}
+      { loading && <div className="loader">Loading...</div>},
+      { !loading && businesses.map((b) => <RestaurantListItem key={ b.id } { ...b } />) }
     </div>
   );
 }
-
 export default App;
